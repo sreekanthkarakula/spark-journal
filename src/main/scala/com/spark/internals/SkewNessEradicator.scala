32f2096425639cdfe99bf444ad10fbaa017173e6
Sreekanth reddy Karakula <sreekanthreddykarakula@MacBook-Pro.local>
2020-06-18 23:11:07 -0700

    new commit

--- src/main/scala/com/spark/internals/SkewNessEradicator.scala
+++ src/main/scala/com/spark/internals/SkewNessEradicator.scala
@@ -6,7 +6,10 @@ object SkewNessEradicator {
 
   implicit class EnhancedDF(df: DataFrame){
 
-    def skewEradicateJoin(df: DataFrame, lookup: DataFrame, joinExpr:Column, typeofColumn: String ) = {
+    def skewEradicateJoin(df: DataFrame, lookup: DataFrame, joinExpr:Column, typeofColumn: String, partitionsize: Integer ) = {
+
+      val df_enhanced = lookup.withColumn("skeu_id", Array(Range(0, partitionsize).toList.map(lit): _* ))
+
 
 
 
